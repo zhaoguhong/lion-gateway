68123a22ff812548b4424340e16c94f72a796e3b
zhaoguhong <zhaobaymax@163.com>
2021-11-19 12:57:25 +0800

    init

---
+++ lion-gateway-server/src/main/java/com/zhaoguhong/lion/gateway/LionGatewayServerApplication.java
@@ -0,0 +1,13 @@
+package com.zhaoguhong.lion.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LionGatewayServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LionGatewayServerApplication.class, args);
+  }
+
+}
---
+++ lion-gateway-server/src/test/java/com/zhaoguhong/lion/gateway/LionGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhaoguhong.lion.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LionGatewayServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
